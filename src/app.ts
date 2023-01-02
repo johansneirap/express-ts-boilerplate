8cc3eed8864623843fdf1e7dfcbf8e9cf2ff72ed
Johans Neira <johansneirap@gmail.com>
2023-01-02 19:14:51 -0300

    init typescript

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+import express from "express";
+const app = express();
+app.use(express.json()); //middleware to parse req body to json
+
+const PORT = 3000;
+
+app.get("/ping", (_req, res) => {
+  res.send("pong");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
---
+++ src/routes/index.ts
@@ -0,0 +1,8 @@
+import express from "express";
+const router = express.Router();
+
+router.get("/", (_req, res) => {
+  res.status(200).json({ message: "Hello World!" });
+});
+
+export default router;
